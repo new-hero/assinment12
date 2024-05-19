e2a6465d8678480d44829790266d63e47fecaa29
Subrata Sarkar <tccdcc8@gmail.com>
2024-05-19 19:35:33 +0600

    user post route add

--- src/App.js
+++ src/App.js
@@ -1,78 +1,119 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Components/Home/Home'
-import Header from './Components/Header/Header';
-import Protfolio from './Components/Protfolio/Protfolio';
-import auth from './firebase.init';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import Login from './Components/Login/Login';
-import Register from './Components/Register/Register';
-import Notfound from './Components/Notfound/Notfound';
-import Private from './Components/Private/Private';
-import Loading from './Components/Loading/Loading';
-import Purchase from './Components/Purchase/Purchase';
-import Dashbord from './Components/Dashbord/Dashbord';
-import Blogs from './Components/Blogs/Blogs';
-import Tools from './Components/Tools/Tools';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Review from './Components/Reviews/Review';
-import Profile from './Components/Profile/Profile';
-import Myorder from './Components/Myorder/Myorder';
-import Allusers from './Components/Dashbord/Allusers';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+import Protfolio from "./Components/Protfolio/Protfolio";
+import auth from "./firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import Notfound from "./Components/Notfound/Notfound";
+import Private from "./Components/Private/Private";
+import Loading from "./Components/Loading/Loading";
+import Purchase from "./Components/Purchase/Purchase";
+import Dashbord from "./Components/Dashbord/Dashbord";
+import Blogs from "./Components/Blogs/Blogs";
+import Tools from "./Components/Tools/Tools";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Review from "./Components/Reviews/Review";
+import Profile from "./Components/Profile/Profile";
+import Myorder from "./Components/Myorder/Myorder";
+import Allusers from "./Components/Dashbord/Allusers";
 // import Managetool from './Components/Managetools/Managetool';
-import Managetools from './Components/Managetools/Managetools';
-import Managetool from './Components/Managetools/Managetool';
-import Allorders from './Components/Allorders/Allorders';
-import AddTool from './Components/AddTool/AddTool';
-
-
+import Managetools from "./Components/Managetools/Managetools";
+import Managetool from "./Components/Managetools/Managetool";
+import Allorders from "./Components/Allorders/Allorders";
+import AddTool from "./Components/AddTool/AddTool";
 
 function App() {
-
-  const [user, loading, error] = useAuthState(auth)
+  const [user, loading] = useAuthState(auth);
 
   if (loading) {
-    return <Loading></Loading>
+    return <Loading></Loading>;
+  }
+  if (user) {
+    fetch("https://assignment12server-lime.vercel.app/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   }
 
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-        <Route path='/tools' element={<Tools></Tools>}></Route>
-        <Route path='/protfolio' element={<Protfolio></Protfolio>}></Route>
-        <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='/tools' element={<Tools></Tools>}></Route>
-        <Route path='/tools/:id' element={<Private>
-          <Purchase></Purchase>
-        </Private>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/tools" element={<Tools></Tools>}></Route>
+        <Route path="/protfolio" element={<Protfolio></Protfolio>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/tools" element={<Tools></Tools>}></Route>
+        <Route
+          path="/tools/:id"
+          element={
+            <Private>
+              <Purchase></Purchase>
+            </Private>
+          }
+        ></Route>
 
-        <Route path='/manage' element={<Private>
-          <Managetools></Managetools>
-        </Private>}></Route>
-        
+        <Route
+          path="/manage"
+          element={
+            <Private>
+              <Managetools></Managetools>
+            </Private>
+          }
+        ></Route>
 
-        <Route path='/addTool' element={<Private>
-          <AddTool/>
-        </Private>}></Route>
+        <Route
+          path="/addTool"
+          element={
+            <Private>
+              <AddTool />
+            </Private>
+          }
+        ></Route>
 
-        <Route path='/manage/:id' element={<Private>
-          <Managetool></Managetool>
-        </Private>}></Route>
+        <Route
+          path="/manage/:id"
+          element={
+            <Private>
+              <Managetool></Managetool>
+            </Private>
+          }
+        ></Route>
 
-        <Route path='/dashbord' element={<Private>
-          <Dashbord /></Private>}>
+        <Route
+          path="/dashbord"
+          element={
+            <Private>
+              <Dashbord />
+            </Private>
+          }
+        >
           <Route index element={<Profile></Profile>}></Route>
-          <Route path='/dashbord/review' element={<Review></Review>}> </Route>
-          <Route path='/dashbord/allorders' element={<Allorders></Allorders>}> </Route>
-          <Route path='/dashbord/allusers' element={<Allusers></Allusers>}> </Route>
-          <Route path='/dashbord/orders' element={<Myorder />} > </Route>
+          <Route path="/dashbord/review" element={<Review></Review>}>
+            {" "}
+          </Route>
+          <Route path="/dashbord/allorders" element={<Allorders></Allorders>}>
+            {" "}
+          </Route>
+          <Route path="/dashbord/allusers" element={<Allusers></Allusers>}>
+            {" "}
+          </Route>
+          <Route path="/dashbord/orders" element={<Myorder />}>
+            {" "}
+          </Route>
         </Route>
-        <Route path='*' element={<Notfound></Notfound>}></Route>
+        <Route path="*" element={<Notfound></Notfound>}></Route>
       </Routes>
       <ToastContainer></ToastContainer>
     </div>
